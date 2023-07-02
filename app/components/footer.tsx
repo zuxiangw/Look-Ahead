b57e9496a42d646af3b2e361d4cc9bf8282726dd
zuxiangw <zuxiangwang2002@gmail.com>
2023-07-01 20:32:57 -0400

    Home page basic completed. Footer content added
    Home page basic content's functionality has been implemented. Need to add the interaction of clicking from a search result in the search bar and directed to a corresponding place page. Need to add the interaction of clicking on a title of a recommendation place and is directed to a corresponding place page.

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,5 +1,21 @@
 const Footer = () => {
-  return <div className="h-96 bg-sky-400"></div>;
+  return (
+    <div className="h-64 bg-sky-400 grid grid-col-3">
+      <div className="h-full col-start-1 p-8 flex justify-center tracking-wider">
+        <h1 className="text-3xl font-bold">Look Ahead</h1>
+      </div>
+      <div className="h-full col-start-2 p-8 flex flex-col items-center tracking-wider">
+        <h1 className="text-3xl font-bold">Legal</h1>
+        <p className="mt-8">Terms of Use</p>
+        <p className="mt-8">Privacy Policy</p>
+      </div>
+      <div className="h-full col-start-3 p-8 flex flex-col items-center tracking-wider">
+        <h1 className="text-3xl font-bold">Contacts</h1>
+        <p className="mt-8">lookahead@gmail.com</p>
+        <p className="mt-8">1(234)-567-8912</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
