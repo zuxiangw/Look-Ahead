d65ce33c0c6072df17ab580d98b64588dbde0ba7
zuxiangw <zuxiangwang2002@gmail.com>
2023-08-28 23:56:31 -0400

    test route formal

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,6 +1,17 @@
 import { insertUser } from "@/app/utils/database";
 import { NextResponse } from "next/server";
 export async function GET() {
-  await insertUser("testing", "testing", "test", undefined);
-  return new NextResponse(JSON.stringify({ status: 200, message: "Success!" }));
+  try {
+    await insertUser("testing", "testing", "test", undefined);
+    return new NextResponse(
+      JSON.stringify({ status: 200, message: "Success!" })
+    );
+  } catch (error) {
+    return new NextResponse(
+      JSON.stringify({
+        status: 500,
+        message: `Error: ${(error as Error).toString()}`,
+      })
+    );
+  }
 }
