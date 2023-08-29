61a251449e9542ac2890c00d9db85d56c2c0911f
zuxiangw <zuxiangwang2002@gmail.com>
2023-08-29 12:20:43 -0400

    closePool tesitng route removed

--- app/api/test/closePool/route.ts
+++
@@ -1,8 +0,0 @@
-import { closePool } from "@/app/utils/database";
-import { NextResponse } from "next/server";
-export async function GET() {
-  await closePool();
-  return new NextResponse(
-    JSON.stringify({ status: 200, message: "Success Close!" })
-  );
-}
