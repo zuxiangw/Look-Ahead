32eb7cae53098aa5f9fcf5cfbae1944978a7727b
zuxiangw <zuxiangwang2002@gmail.com>
2023-08-30 17:48:41 -0400

    reviews routing changed

--- app/(pages)/home/reviews/page.tsx
+++ app/(pages)/home/reviews/page.tsx
@@ -1,6 +1,7 @@
 import HeaderUnderbar from "@/app/components/headerUnderbar";
 import ReviewsManager from "../../../components/placeComponents/ReviewsManager";
 import { GET_reviews } from "@/app/utils/routeHandlers/reviews";
+import { getAllReviews } from "@/app/utils/database";
 
 const Page = async () => {
   const reviews = await fetchAllReviews();
@@ -14,9 +15,10 @@ const Page = async () => {
 };
 
 const fetchAllReviews = async () => {
-  const res = await GET_reviews(null);
+  const res = await fetch("https://look-ahead.vercel.app/api/reviews", {
+    next: { revalidate: 0 },
+  });
   if (res.ok) {
-    console.log("Success");
     const reviews = (await res.json()).data;
     return reviews.map((review: any) => {
       return {
@@ -25,8 +27,6 @@ const fetchAllReviews = async () => {
       };
     });
   } else {
-    console.log("Fail");
-    console.log(res);
     return [];
   }
 };
